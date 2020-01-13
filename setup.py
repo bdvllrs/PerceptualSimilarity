cb4cdb33ffd74c07e38f22e2464b83af6c83863e
Benjamin <benjamin.devillers@supelec.fr>
2020-01-13 17:56:50 +0100

    Add package data.

--- setup.py
+++ setup.py
@@ -5,4 +5,8 @@ setup(name='perceptual_similarity',
       install_requires=["torch>=0.4.0", "torchvision>=0.2.1", "numpy>=1.14.3",
                         "scipy>=1.0.1", "scikit-image>=0.13.0", "opencv-python>=2.4.11",
                         "matplotlib>=1.5.1", "tqdm>=4.28.1"],
-      packages=find_packages())
+      packages=find_packages(),
+      package_data={'perceptual_similarity': ['weights/v0.0/*',
+                                              'weights/v0.1/*']},
+      include_package_data=True,
+      )
