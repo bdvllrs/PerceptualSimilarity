5f1c095f353f1c5463fec71444fc84fb907da38a
Benjamin <bdvllrs@gmail.com>
2020-03-05 14:11:14 +0100

    Fix resnet model.

--- perceptual_similarity/networks_basic.py
+++ perceptual_similarity/networks_basic.py
@@ -47,7 +47,7 @@ class PNetLin(nn.Module):
             self.chns = [64,128,256,384,384,512,512]
         elif(self.pnet_type=='resnet'):
             net_type = pn.resnet
-            self.chns = [64,64,128,256,512]
+            self.chns = [64,256,512,1024,2048]
         self.L = len(self.chns)
 
         self.net = net_type(pretrained=not self.pnet_rand, requires_grad=self.pnet_tune)
